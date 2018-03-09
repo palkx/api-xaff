8990d7a61dd984148717f5e081e26ebb5d041744
Mikhail K <sm1leua@ya.ru>
2018-03-09 17:11:03 +0200

    Updated deps and added randAnime command

--- server/app/telegram/telegrambot.ts
+++ server/app/telegram/telegrambot.ts
@@ -1,3 +1,6 @@
+import { request } from "request-promise";
+const { version } = require("../../../package.json");
+
 export default function TelegramBot(telegrafApp) {
   const me = this;
   me.telegrafApp = telegrafApp;
@@ -6,6 +9,7 @@ export default function TelegramBot(telegrafApp) {
 
   telegrafApp.command('start', me.onStartCommand.bind(me));
   telegrafApp.command('cat', me.onCatCommand.bind(me));
+  telegrafApp.command('randAnime', me.onRandomAnimeCommand.bind(me));
   telegrafApp.command('myid', me.onMyIdCommand.bind(me));
 }
 
@@ -16,6 +20,17 @@ TelegramBot.prototype = {
   onCatCommand(ctx) {
     const cat = 'http://thecatapi.com/api/images/get?format=src&type=png&size=med';
     return ctx.replyWithPhoto({ url: cat });
+  },
+  async onRandomAnimeCommand(ctx) {
+    const animeImage = await request({
+      uri: `https://yande.re/post.json?page=${Math.floor((Math.random() * 8800) + 1)}`,
+      headers: {'User-Agent': `XaFF Telegram Bot v${version} (https://bitbucket.org/xaff/api/)` },
+      json: true
+    });
+    if (animeImage.length === 0) {
+      return ctx.reply('Oooppss. I am getting some error, sorry. I will fix it as fast as i can.')
+    }
+    return ctx.replyWithPhoto({ url: animeImage[0].file_url});
   },
   onMyIdCommand(ctx) {
     return ctx.reply(ctx.from.id);
