ad83ce6ea10457f166392463a0c9d745af180526
Mikhail K <sm1leua@ya.ru>
2018-03-25 12:12:44 +0300

    updated user model

--- server/app/models/user.ts
+++ server/app/models/user.ts
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, lowercase: true, trim: true, required: true },
   password: { type: String, required: true },
-  access: [{ service: String, level: Number }],
+  roles: [ String ],
+  updated: { type: Date, default: Date.now() },
   created: { type: Date, default: Date.now() }
 });
 
