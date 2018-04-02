8df4971433413f5240567a91f27391e8b60b36e9
Mikhail K <sm1leua@ya.ru>
2018-04-02 20:10:02 +0300

    added update token route

--- server/app/controllers/user.ts
+++ server/app/controllers/user.ts
@@ -27,4 +27,14 @@ export default class UserCtrl extends BaseCtrl {
     });
   }
 
+  updateToken = (req, res) => {
+    console.log(req.decoded);
+    jwt.sign( req.decoded.user, privateKey, { algorithm: 'RS256', expiresIn: 7 * 24 * 60 * 60 }, (e, token) => {
+      if (e) {
+        console.log(e);
+        res.status(500);
+      }
+      res.status(200).json({ token: token });
+    });
+  }
 }
